92525e98c03f08d74524382ec0eec9b2fde59639
JorgeVallejos14 <jorge.vallejos14@inacapmail.cl>
2026-06-21 22:46:21 -0400

    feat: proyecto base del plantel

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { players } from "./data/players";
+
+function App() {
+	console.log(players);
+
+	return <h1>Mobius FC</h1>;
+}
+
+export default App;
---
+++ src/data/players.js
@@ -0,0 +1,20 @@
+export const players = [
+  {
+    id: 1,
+    name: "Sonic",
+    position: "Delantero",
+    number: 9
+  },
+  {
+    id: 2,
+    name: "Tails",
+    position: "Portero",
+    number: 1
+  },
+  {
+    id: 3,
+    name: "Knuckles",
+    position: "Defensa",
+    number: 4
+  }
+];
